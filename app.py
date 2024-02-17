e3b6e48e7fcd385aba3c1840f9ce9f673147d01b
Mykolas Jerutis <mykolasjerutis@email.com>
2024-02-17 15:07:20 +0200

    Add possible login funcitonality for future

---
+++ app.py
@@ -0,0 +1,114 @@
+import os
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+
+from flask_bcrypt import Bcrypt
+from flask_login import (
+    LoginManager,
+    current_user,
+    logout_user,
+    login_user,
+    UserMixin,
+    login_required,
+)
+import forms
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "4654f5dfadsrfasdr54e6rae"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "biudzetas.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "prisijungti"
+login_manager.login_message_category = "info"
+
+
+class UserRole(Enum):
+    CLIENT = "client"
+    EMPLOYEE = "employee"
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column("name", db.String(20), unique=True, nullable=False)
+    email = db.Column("Email address", db.String(120), unique=True, nullable=False)
+    password = db.Column("Password", db.String(60), unique=True, nullable=False)
+    statusas = db.Column(db.String(10), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    db.create_all()
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        coded_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            password=coded_password,
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration was successful! You may login", "success")
+        return redirect(url_for("index"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("index"))
+        else:
+            flash("Failed to login, please check your details again", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
+
+
+# @app.route("/irasai")
+# @login_required
+# def irasai():
+#     return render_template("irasai.html", title="Įrašai")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
+    db.create_all()
---
+++ forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    SubmitField,
+    BooleanField,
+    StringField,
+    PasswordField,
+    SelectField,
+    Select,
+)
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+import app
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Name", [DataRequired()])
+    email = StringField("Email", [Email()])
+    status = SelectField(
+        "Status",
+        choices=[("client", "Client"), ("employee", "Employee")],
+        validators=[DataRequired()],
+    )
+    password = PasswordField("Password", [DataRequired()])
+    confirmed_password = PasswordField(
+        "Repeat Password", [EqualTo("password", "Passwords must match.")]
+    )
+    submit = SubmitField("Register")
+
+    def check_name(self, name):
+        user = app.User.query.filter_by(vardas=name.data).first()
+        if user:
+            raise ValidationError(
+                "Name is already used, please choose a different name."
+            )
+
+    def check_email(self, el_pastas):
+        user = app.User.query.filter_by(el_pastas=el_pastas.data).first()
+        if user:
+            raise ValidationError(
+                "This email is already used, please choose a different email or try to login."
+            )
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
+    remember = BooleanField("Remember me")
+    status = SelectField(
+        "Status",
+        choices=[("client", "Client"), ("employee", "Employee")],
+        validators=[DataRequired()],
+    )
+    submit = SubmitField("Login")
